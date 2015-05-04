5e588de475a24dc8fb8b4fc13578c7f103240d08
Ildar Safin <safild94@gmail.com>
2015-05-04 22:16:28 +0300

    Add rspec configs

--- db/migrate/20150504140531_create_notes.rb
+++ db/migrate/20150504140531_create_notes.rb
@@ -1,8 +1,10 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
-
+      t.string :content, null: false
       t.timestamps null: false
     end
+
+    add_index :notes, :content, unique: true
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
-require 'spec_helper'
+
 require File.expand_path('../../config/environment', __FILE__)
 
 require 'rspec/rails'
 require "shoulda/matchers"
 
-ActiveRecord::Migration.maintain_test_schema!
-
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
 
 module Features
   # Extend this module in spec/support/features/*.rb
@@ -14,26 +13,13 @@ module Features
 end
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Features, type: :feature
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+ActiveRecord::Migration.maintain_test_schema!
